994fcf8249e376eb33cad555c20e5f84ca04bf87
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2025-12-09 21:25:28 -0600

    Basic Start Menu Interface

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.awt.Color;
+
+import edu.macalester.graphics.*;
+import edu.macalester.graphics.ui.Button;;
+
 public class Main {
+    private final static int WINDOW_WIDTH = 300;
+    private final static int WINDOW_HEIGHT = 100;
+    private static CanvasWindow canvas;
+    private static Button knuthButton;
+    private static Button playButton;
+
     public static void main(String[] args) {
+        startMenu();
+    }
+
+    private static void startMenu() {
+        if (canvas != null) {
+            canvas.closeWindow();
+        }
+        canvas = new CanvasWindow("Start Menu", WINDOW_WIDTH, WINDOW_HEIGHT);
+        canvas.setBackground(Color.LIGHT_GRAY);
+        addButtons();
+        canvas.draw();
+    }
+
+    private static void addButtons() {
+        knuthButton = new Button("Run Knuth Algorithm");
+        playButton = new Button("Play Mastermind");
+        knuthButton.setPosition(WINDOW_WIDTH/2 - knuthButton.getWidth()/2,WINDOW_HEIGHT/2 - knuthButton.getHeight()*1.2);
+        playButton.setPosition(WINDOW_WIDTH/2 - playButton.getWidth()/2, knuthButton.getY() + knuthButton.getHeight() + 10);
+        canvas.add(knuthButton);
+        canvas.add(playButton);
+        knuthButton.onClick(() -> runKnuth());
+        playButton.onClick(() -> playMastermind());
+    }
+
+    private static void runKnuth() {
         KnuthAlgorithm kn = new KnuthAlgorithm();
         kn.runGame();
+    }
+
+    private static void playMastermind() {
+        Mastermind.main(null);
     }
 }
